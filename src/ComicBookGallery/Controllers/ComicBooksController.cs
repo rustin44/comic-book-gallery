0416b6a0eaad5f6aff11f7c7a579f214b87fd2f0
Rusty Barrett <rustin44@gmail.com>
2017-03-25 22:35:25 -0500

    Moved data to Controller

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,6 +8,24 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            ViewBag.SeriesTitle = "The Amazing Spiderman";
+            ViewBag.IssueNumbr = 700;
+            ViewBag.Description = "<p>Final issue! Witness some stuff!</p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Whathisnaney",
+                "Colors: Edgar Yost",
+                "Letters: Chris Eliopoulus"
+            };
 
+
+            return View();
+        }
+            
+        
     }
 }
